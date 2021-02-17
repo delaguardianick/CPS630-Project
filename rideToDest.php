dfc7a462e423c079cadcac0ef9965e1b2b1a6339
delaguardianick <delaguardianick@gmail.com>
2021-02-16 21:30:41 -0500

    Changed to OSM and OpenLayer API

--- rideToDest.php
+++ rideToDest.php
@@ -3,6 +3,8 @@
         <title>Ride And Deliver - Service A</title>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <link rel="stylesheet" href="style.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="OpenLayers.js"></script>
         <script src="maps.js"></script>
     </head>
     <body>
@@ -13,11 +15,14 @@
     <h5>Ride to a destination from this source (inside the city or out of it max 50km far)</h5>
     <br>
     <input type="text" name="origin" id="origin" placeholder="Origin">
+    <button type="button" id="find-me" >Use Location</button>
+    <br>
     <input type="text" name="destination" id="destination" placeholder="Destination">
-    <!-- <button type="button" onclick="initMap()">Search</button> -->
-    <div id="map"></div>
-        
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCIumcSOTeP890tfGtNPajH0WmErIjAgcM&callback=initMap&libraries=&v=weekly"></script>
+    <button type="button" id="show-map" >Search</button>
+    <p id = "status"></p>
+    <a id = "map-link" target="_blank"></a>
+    <div id="Map" style="height:250px"></div>
+    
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
