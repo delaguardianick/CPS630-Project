23f07c7d46dbfa0dfb8f788dba5dab527a938d1b
delaguardianick <delaguardianick@gmail.com>
2021-02-28 13:43:26 -0500

    geocoding destination

---
+++ maps.js
@@ -0,0 +1,86 @@
+var origin = "";
+var destination = "";
+var destCoords;
+
+function geoFindMe() {
+    const status = document.querySelector('#status');
+    const mapLink = document.querySelector('#map-link');
+  
+    mapLink.href = '';
+    mapLink.textContent = '';
+  
+    function success(position) {
+      const latitude  = position.coords.latitude;
+      const longitude = position.coords.longitude;
+      origin = [latitude,longitude];
+  
+      status.textContent = '';
+      mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
+      mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
+      document.querySelector('#origin').value = latitude + "," + longitude;
+    }
+  
+    function error() {
+      status.textContent = 'Unable to retrieve your location';
+    }
+    if(!navigator.geolocation) {
+      status.textContent = 'Geolocation is not supported by your browser';
+    } else {
+      status.textContent = 'Locating…';
+      navigator.geolocation.getCurrentPosition(success, error);
+    }
+  
+  }
+
+  // Initially map will be blank.
+  // Press "Use my Current Location" and the input box will fill with users coordinates
+  // Then press search
+function initMap()
+{
+    var location = {lat: origin[0], lng: origin[1]};
+    // location = {lat: 43.7575, lng: -79.3910};
+
+    var destination = document.querySelector("#destination").value;
+    destCoords = geocode(destination).then(console.log); 
+    console.log(destCoords);
+
+    var location2 = {lat: destCoords[0], lng: destCoords[1]};
+
+    var map = new google.maps.Map(document.getElementById("map"),
+        { zoom: 12, center: location});
+
+    var marker = new google.maps.Marker(
+        {position: location, map: map});    
+    var infoWindow = new google.maps.InfoWindow(
+        { content: "<h5> source </h5>"});
+    marker.addListener("click", function()
+        { infoWindow.open(map, marker); });
+
+
+    var marker2 = new google.maps.Marker(
+        {position: location2, map: map});    
+    var infoWindow2 = new google.maps.InfoWindow(
+        { content: "<h5> dest </h5>"});
+    marker2.addListener("click", function()
+        { infoWindow2.open(map, marker2); });
+
+    console.log(destCoords);
+}
+
+async function geocode(address){
+  address = address.replace(/\s/g, "+");
+  url = `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=AIzaSyCIumcSOTeP890tfGtNPajH0WmErIjAgcM`
+  console.log(url);
+  $.get(url, function(data,status){
+    var lat = data.results[0].geometry.location.lat;
+    var lng = data.results[0].geometry.location.lng;
+    destCoords =  [lat,lng];
+    return destCoords;
+  })
+}
+
+$(document).ready(function (){
+    document.querySelector('#find-me').addEventListener('click', geoFindMe);
+    document.querySelector('#show-map').addEventListener('click', initMap);
+    document.querySelector('#test').addEventListener('click', geocode);
+})
--- rideToDest.php
+++ rideToDest.php
@@ -4,8 +4,7 @@
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <link rel="stylesheet" href="style.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-        <!-- <script src="OpenLayers.js"></script> -->
-        <script src="maps2.js"></script>
+        <script src="maps.js"></script>
     </head>
     <body>
     <nav>
@@ -19,12 +18,13 @@
     <br>
     <input type="text" name="destination" id="destination" placeholder="Destination">
     <button type="button" id="show-map" >Search</button>
+    <button type="button" id="test" >Test</button>
     <p id = "status"></p>
     <a id = "map-link" target="_blank"></a>
     <div id="map"> </div>
     
     <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCIumcSOTeP890tfGtNPajH0WmErIjAgcM&callback=initMap"></script>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <!-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script> -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     </body>
