1781668cbaad0ff0dc556de998f45d40aaa9ea50
delaguardianick <delaguardianick@gmail.com>
2021-02-28 23:32:47 -0500

    Added a line between markers and distance check

--- maps.js
+++ maps.js
@@ -33,53 +33,87 @@ function geoFindMe() {
   
   }
 
+    
+  function haversine_distance(mk1, mk2) {
+    var R = 6371.0710; // Radius of the Earth in kilometers
+    var rlat1 = mk1.position.lat() * (Math.PI/180); // Convert degrees to radians
+    var rlat2 = mk2.position.lat() * (Math.PI/180); // Convert degrees to radians
+    var difflat = rlat2-rlat1; // Radian difference (latitudes)
+    var difflon = (mk2.position.lng()-mk1.position.lng()) * (Math.PI/180); // Radian difference (longitudes)
+
+    var d = 2 * R * Math.asin(Math.sqrt(Math.sin(difflat/2)*Math.sin(difflat/2)+Math.cos(rlat1)*Math.cos(rlat2)*Math.sin(difflon/2)*Math.sin(difflon/2)));
+    return d;
+  }
+
   function initMap()
   {
-      // var mapOrigin = {lat: origin[0], lng: origin[1]};
       // mapOrigin = {lat: 43.7575, lng: -79.3910};
 
-      var origin = document.querySelector("#origin").value;
+      // Gets address inputted in 'origin' text box
+      var origin = document.querySelector("#origin").value; 
 
+      // Calls func geocode with the plain text address, returns coordinates
+      // Since geocode has an asynchronous api call, 
+      // Promise is used to wait for the data of the fetch.
       geocode(origin).then(data => {
         var lat = data.results[0].geometry.location.lat;
         var lng = data.results[0].geometry.location.lng;
         originCoords =  [lat,lng];
+
         console.log("Origin coods: " + originCoords);
         mapOrigin = {lat: originCoords[0], lng: originCoords[1]};
       
-
-      var destination = document.querySelector("#destination").value;
-  
-      geocode(destination).then(data => {
-        var lat = data.results[0].geometry.location.lat;
-        var lng = data.results[0].geometry.location.lng;
-        destCoords =  [lat,lng];
-        mapDestination = {lat: destCoords[0], lng: destCoords[1]};
+        var destination = document.querySelector("#destination").value;
+    
+        geocode(destination).then(data => {
+          var lat = data.results[0].geometry.location.lat;
+          var lng = data.results[0].geometry.location.lng;
+          destCoords =  [lat,lng];
+          mapDestination = {lat: destCoords[0], lng: destCoords[1]};
+        
+          console.log(destCoords);  
       
-      console.log(destCoords);  
-  
-      var map = new google.maps.Map(document.getElementById("map"),
-          { zoom: 12, center: mapOrigin});
-  
-      if (typeof mapOrigin !== 'undefined'){
-      var marker = new google.maps.Marker(
-          {position: mapOrigin, map: map});    
-      var infoWindow = new google.maps.InfoWindow(
-          { content: "<h5> source </h5>"});
-      marker.addListener("click", function()
-          { infoWindow.open(map, marker); });
-      }
-  
-      if (typeof mapDestination !== 'undefined'){
-        var marker2 = new google.maps.Marker(
-            {position: mapDestination, map: map});    
-        var infoWindow2 = new google.maps.InfoWindow(
-            { content: "<h5> dest </h5x>"});
-        marker2.addListener("click", function()
-            { infoWindow2.open(map, marker2); });
+          var map = new google.maps.Map(document.getElementById("map"),
+              { zoom: 12,
+                center: mapOrigin,
+              });
+      
+          if (typeof mapOrigin !== 'undefined'){
+            var marker = new google.maps.Marker(
+                {position: mapOrigin, map: map});    
+            var infoWindow = new google.maps.InfoWindow(
+                { content: "<h5> source </h5>"});
+            marker.addListener("click", function()
+                { infoWindow.open(map, marker); });
           }
-        console.log(destCoords);
-      });
+      
+          if (typeof mapDestination !== 'undefined'){
+            var marker2 = new google.maps.Marker(
+                {position: mapDestination, map: map});    
+            var infoWindow2 = new google.maps.InfoWindow(
+                { content: "<h5> dest </h5x>"});
+            marker2.addListener("click", function()
+                { infoWindow2.open(map, marker2); });
+              }
+
+            console.log(destCoords);
+          
+            // Line and distance between markers
+            var line = new google.maps.Polyline({path: [mapOrigin, mapDestination], map: map});
+            var radius = haversine_distance(marker, marker2);
+            console.log(radius);
+            document.getElementById('radius').innerHTML = "Distance: " + radius.toFixed(2) + " km.";
+
+            // Has to be under 50km
+            if (radius <= 50)
+            {
+              document.getElementById('radius').innerHTML += "<br>Locations within range. Proceed for checkout!";
+            }
+            else {
+              document.getElementById('radius').innerHTML += "<br>Locations not within range. Please reduce distance!";
+            }
+
+        });
     });
   }
   
--- rideToDest.php
+++ rideToDest.php
@@ -22,10 +22,11 @@
         <button type="button" id="test" >Test</button>
         <p id = "status"></p>
         <a id = "map-link" target="_blank"></a>
+        <p id="radius"></p>
     </div>
-        <div id="map"> </div>
+    <div id="map"> </div>
     
-    <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCIumcSOTeP890tfGtNPajH0WmErIjAgcM&callback=initMap"></script>
+    <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCIumcSOTeP890tfGtNPajH0WmErIjAgcM&map_ids-6789a6679abe1ef1&callback=initMap"></script>
     <!-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script> -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
