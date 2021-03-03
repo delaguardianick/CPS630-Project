3bab45e4918caf68e2604e5bbbed375f8d795760
alifiyahhussain <31748148+alifiyahhussain@users.noreply.github.com>
2021-03-03 18:17:41 -0500

    Update navigation.php

--- navigation.php
+++ navigation.php
@@ -3,7 +3,6 @@
 ?>
 <head>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  <link rel="stylesheet" href="navbar.ccs">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js%22%3E"></script>
 </head>
 <nav class="navbar navbar-expand-lg navbar-light" id="nav-bar">
